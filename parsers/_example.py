dec96d9a0ca4b29dec5ed40c59e6486d662f404d
Tyler <gibbstyler7@gmail.com>
2019-07-12 03:20:23 -0500

    theory craft how custom parsers work

---
+++ parsers/_example.py
@@ -0,0 +1,23 @@
+class Example:
+    def __init__(self):
+        """
+        Change 'self.url' to be the URL of the desired
+        RSS feed to parse.
+        """
+        self.url = "http://www.example.com/rss"
+
+    def get_link_location(self, item):
+        """
+        Beginning from the <item> object, direct the
+        script to where to find either:
+        a.) the magnet URL
+        b.) the torrent file
+
+        Either of these two options will be parsed correctly
+        by Tsundoku. If any manipulation needs to take place,
+        please do that here.
+        """
+        return item["link"]
+
+def setup():
+    return Example()
---
+++ parsers/horriblesubs.py
@@ -0,0 +1,23 @@
+class HorribleSubs:
+    def __init__(self):
+        """
+        Change 'self.url' to be the URL of the desired
+        RSS feed to parse.
+        """
+        self.url = "http://www.horriblesubs.info/rss.php?res=720"
+
+    def get_link_location(self, item):
+        """
+        Beginning from the <item> object, direct the
+        script to where to find either:
+        a.) the magnet URL
+        b.) the torrent file
+
+        Either of these two options will be parsed correctly
+        by Tsundoku. If any manipulation needs to take place,
+        please do that here.
+        """
+        return item["link"]
+
+def setup():
+    return HorribleSubs()
