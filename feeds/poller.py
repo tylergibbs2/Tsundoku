2059b348661040249d61c49e52b8f2a8bc4fe2b4
Tyler <gibbstyler7@gmail.com>
2019-07-17 16:06:38 -0500

    implement title matching with desired

--- feeds/poller.py
+++ feeds/poller.py
@@ -1,6 +1,8 @@
 import asyncio
+import typing
 
 import feedparser
+from fuzzywuzzy import process
 from quart.ctx import AppContext
 
 from config import get_config_value
@@ -53,6 +55,42 @@ class Poller:
             await self.check_item(item)
 
 
+    async def check_item_for_match(self, show_name: str, episode: int) -> typing.Optional[str]:
+        """
+        Takes a show name from RSS and an episode from RSS and
+        checks if the object should be downloaded.
+
+        An item should be downloaded only if it is in the
+        `shows` table and the episode is not already in the
+        `show_entry` table.
+
+        Parameters
+        ----------
+        show_name: str
+            The name of the show from RSS.
+        episode: int
+            The episode of the entry from RSS.
+
+        Returns
+        -------
+        typing.Optional[str]
+            The name of the matched show.
+        """
+        async with self.app.db_pool.acquire() as con:
+            desired_shows = await con.fetch("""
+                SELECT search_title FROM shows;
+            """)
+        show_list = [show["search_title"] for show in desired_shows]
+        
+        if not show_list:
+            return
+
+        match = process.extractOne(show_name, show_list)
+
+        if match[1] >= 90:
+            return match[0]
+
+
     async def check_item(self, item: dict) -> None:
         """
         Checks an item to see if it is from a 
@@ -68,11 +106,6 @@ class Poller:
             if self.current_parser.ignore_logic(item) == False:
                 return
 
-        if hasattr(self.current_parser, "get_link_location"):
-            torrent_url = await self.get_torrent_link(item)
-        else:
-            torrent_url = item["link"]
-
         if hasattr(self.current_parser, "get_file_name"):
             torrent_name = self.current_parser.get_file_name(item)
         else:
@@ -80,6 +113,16 @@ class Poller:
 
         show_name = self.current_parser.get_show_name(torrent_name)
         show_episode = self.current_parser.get_episode_number(torrent_name)
+
+        match = await self.check_item_for_match(show_name, show_episode)
+
+        if match is None:
+            return
+
+        if hasattr(self.current_parser, "get_link_location"):
+            torrent_url = await self.get_torrent_link(item)
+        else:
+            torrent_url = item["link"]
 
         print(show_name, show_episode)
 
@@ -118,4 +161,3 @@ class Poller:
         torrent_location = self.current_parser.get_link_location(item)
 
         return await deluge.get_magnet(torrent_location)
-
