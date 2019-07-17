0a3ec372cbd45790301f986c8890b30a60e6b25e
Tyler <gibbstyler7@gmail.com>
2019-07-17 01:27:28 -0500

    implement basic database schema

---
+++ create_db.sql
@@ -0,0 +1,17 @@
+CREATE TYPE show_state AS ENUM ('new', 'downloading', 'seeding', 'complete');
+
+CREATE TABLE IF NOT EXISTS shows (
+    id BIGSERIAL PRIMARY KEY,
+    search_title TEXT NOT NULL,
+    desired_format TEXT,
+    desired_folder TEXT,
+    season SMALLINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS show_entry (
+    id BIGSERIAL PRIMARY KEY,
+    show_id BIGSERIAL NOT NULL REFERENCES shows(id),
+    episode SMALLINT NOT NULL,
+    current_state show_state NOT NULL,
+    torrent_hash TEXT
+);
