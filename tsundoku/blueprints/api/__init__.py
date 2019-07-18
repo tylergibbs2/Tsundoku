b11d1da1eaa622595e1e4961bfb18051a2fef8b0
Tyler <gibbstyler7@gmail.com>
2019-07-18 15:04:11 -0500

    follow REST naming conventions

--- tsundoku/blueprints/api/__init__.py
+++ tsundoku/blueprints/api/__init__.py
@@ -5,7 +5,7 @@ from quart import current_app as app
 
 api_blueprint = Blueprint('api', __name__, url_prefix="/api")
 
-@api_blueprint.route("/get_shows/", methods=["GET"])
+@api_blueprint.route("/shows", methods=["GET"])
 async def get_shows():
     async with app.db_pool.acquire() as con:
         shows = await con.fetch("""
@@ -15,7 +15,7 @@ async def get_shows():
     return json.dumps([dict(record) for record in shows])
 
 
-@api_blueprint.route("/get_show_by_id/<int:show_id>", methods=["GET"])
+@api_blueprint.route("/shows/<int:show_id>", methods=["GET"])
 async def get_show_by_id(show_id: int):
     async with app.db_pool.acquire() as con:
         show = await con.fetchrow("""
@@ -29,8 +29,8 @@ async def get_show_by_id(show_id: int):
     return json.dumps(dict(show))
 
 
-@api_blueprint.route("/get_show_entries/<int:show_id>", methods=["GET"])
-async def get_show_entries(show_id: int):
+@api_blueprint.route("/shows/<int:show_id>/entries", methods=["GET"])
+async def get_show_entries_by_show_id(show_id: int):
     async with app.db_pool.acquire() as con:
         entries = await con.fetch("""
             SELECT id, episode, current_state, torrent_hash
@@ -40,9 +40,9 @@ async def get_show_entries(show_id: int):
         return json.dumps([dict(record) for record in entries])
 
 
-@api_blueprint.route("/handle_entry/", methods=["POST"])
-async def handle_entry():
-    required_arguments = {"show_id", "episode", "magnet"}
+@api_blueprint.route("/shows/<int:show_id>/entries", methods=["POST"])
+async def post_show_entries(show_id: int):
+    required_arguments = {"episode", "magnet"}
     await request.get_data()
     arguments = await request.form
 
@@ -53,12 +53,6 @@ async def handle_entry():
         return Response(json.dumps(response), status=400)
     
     try:
-        show_id = int(arguments["show_id"])
-    except ValueError:
-        response["error"] = "show_id argument must be int"
-        return Response(json.dumps(response), status=400)
-
-    try:
         episode = int(arguments["episode"])
     except ValueError:
         response["error"] = "episode argument must be int"
@@ -68,3 +62,17 @@ async def handle_entry():
     response["success"] = True
 
     return json.dumps(response)
+
+
+@api_blueprint.route("/shows/<int:show_id>/entries/<int:entry_id>", methods=["GET"])
+async def get_show_entry_by_entry_id(show_id: int, entry_id: int):
+    async with app.db_pool.acquire() as con:
+        entry = await con.fetchrow("""
+            SELECT id, episode, current_state, torrent_hash
+            FROM show_entry WHERE show_id=$1 AND id=$2;
+        """, show_id, entry_id)
+
+        if entry is None:
+            entry = {}
+
+        return json.dumps(dict(entry))
