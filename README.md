905c068ccee1d096a82b006d5ce374cccfc3fca3
Tyler Gibbs <gibbstyler7@gmail.com>
2020-10-03 01:40:56 -0500

    convert default parser to erai raws
    horriblesubs shut down :(

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Tsundoku (積ん読)
 
 Tsundoku is an all-in-one utility to download, rename, and move anime from RSS feeds.
-Anime is able to be matched from any source with an RSS feed. Out of the box, Tsundoku has a parser for HorribleSubs installed.
+Anime is able to be matched from any source with an RSS feed. Out of the box, Tsundoku has a parser for Erai-raws installed.
 
 ## Requirements
 
---
+++ parsers/erairaws.py
@@ -0,0 +1,58 @@
+import re
+
+import anitopy
+
+
+class EraiRaws:
+    def __init__(self, app):
+        """
+        Change 'self.url' and 'self.name' to be the URL
+        and name of the desired RSS feed to parse.
+        """
+        self.name = "Erai-raws"
+        self.url = "https://nyaa.si/?page=rss&u=Erai-raws"
+
+        self.app = app
+
+    def get_show_name(self, file_name: str) -> str:
+        """
+        Using the `file_name` argument, you must parse
+        the file name in order to get the name of the show.
+
+        Failure to do so will result in incorrect matching.
+        """
+        parsed = anitopy.parse(file_name)
+
+        return parsed["anime_title"]
+
+    def get_episode_number(self, file_name: str) -> int:
+        """
+        Using the `file_name` argument, you must parse
+        the file name in order to get the episode of the release.
+
+        Failure to do so will result in incorrect matching.
+        """
+        parsed = anitopy.parse(file_name)
+
+        return int(parsed["episode_number"])
+
+    def ignore_logic(self, item: dict) -> bool:
+        """
+        Optional
+
+        This is the first function a parser runs.
+
+        If this returns False, the item will instantly
+        be ignored by Tsundoku. Any other return value
+        will continue operation as normal.
+        """
+        parsed = anitopy.parse(item["title"])
+
+        title = parsed.get("anime_title")
+        episode = parsed.get("episode_number")
+        if title is None or episode is None:
+            return False
+
+
+def setup(app):
+    return EraiRaws(app)
