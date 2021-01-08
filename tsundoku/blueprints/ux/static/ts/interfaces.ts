197bdada65240c76ea1292cfbfe2e163c0a95cee
Tyler Gibbs <gibbstyler7@gmail.com>
2021-01-07 19:39:48 -0600

    fix interface definitions to use semicolons instead of commas

--- tsundoku/blueprints/ux/static/ts/interfaces.ts
+++ tsundoku/blueprints/ux/static/ts/interfaces.ts
@@ -1,34 +1,34 @@
 interface PartialEntry {
-    id: number,
-    show_id: number,
-    current_state: string,
-    episode: number
+    id: number;
+    show_id: number;
+    current_state: string;
+    episode: number;
 }
 
 interface WebhookBase {
-    name: string,
-    base_id: number,
-    service: string,
-    url: string,
-    content_fmt: string,
-    valid: boolean
+    name: string;
+    base_id: number;
+    service: string;
+    url: string;
+    content_fmt: string;
+    valid: boolean;
 }
 
 interface Webhook {
-    wh_id: number,
-    show_id: number,
-    triggers: string[],
-    base: WebhookBase
+    wh_id: number;
+    show_id: number;
+    triggers: string[];
+    base: WebhookBase;
 }
 
 interface Show {
-    id: number,
-    title: string,
-    desired_format: string,
-    desired_folder: string,
-    season: number,
-    episode_offset: number,
-    kitsu_id: number,
-    entries: PartialEntry[],
-    webhooks: Webhook[]
+    id: number;
+    title: string;
+    desired_format: string;
+    desired_folder: string;
+    season: number;
+    episode_offset: number;
+    kitsu_id: number;
+    entries: PartialEntry[];
+    webhooks: Webhook[];
 }
