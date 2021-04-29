1411604e211394aa5b8403d10068f7da8524a290
Tyler Gibbs <gibbstyler7@gmail.com>
2021-04-29 13:15:34 -0500

    adds api key attribute to authorized users

--- tsundoku/user.py
+++ tsundoku/user.py
@@ -9,22 +9,30 @@ class User(AuthUser):
         super().__init__(auth_id)
         self._resolved = False
         self._username = ""
+        self._api_key = ""
 
     async def _resolve(self) -> None:
         if not self._resolved:
             async with app.acquire_db() as con:
                 await con.execute("""
                     SELECT
-                        username
+                        username,
+                        api_key
                     FROM
                         users
                     WHERE id = ?;
                 """, self.auth_id)
                 user = await con.fetchone()
             self._username = user["username"]
+            self._api_key = user["api_key"]
             self._resolved = True
 
     @property
     async def username(self) -> str:
         await self._resolve()
         return self._username
+
+    @property
+    async def api_key(self) -> str:
+        await self._resolve()
+        return self._api_key
