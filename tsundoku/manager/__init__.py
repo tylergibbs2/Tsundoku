403050f57b9b4e9353949e68e285e7c912f3897c
Tyler <gibbstyler7@gmail.com>
2023-05-16 21:31:02 -0500

    use tuple instead of list for __all__

--- tsundoku/manager/__init__.py
+++ tsundoku/manager/__init__.py
@@ -3,4 +3,4 @@ from .show import Show
 from .seen_release import SeenRelease
 from .show_collection import ShowCollection
 
-__all__ = ["Entry", "EntryState", "SeenRelease", "Show", "ShowCollection"]
+__all__ = ("Entry", "EntryState", "SeenRelease", "Show", "ShowCollection")
