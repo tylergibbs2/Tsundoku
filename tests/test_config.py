0fd0f517cc8cc1555231ce6800222d57324eb228
Tyler <gibbstyler7@gmail.com>
2023-10-22 21:57:43 -0500

    adds test cases for configuration classes

---
+++ tests/test_config.py
@@ -0,0 +1,92 @@
+from __future__ import annotations
+
+import logging
+
+import pytest
+from pytest import LogCaptureFixture
+
+from tests.mock import MockTsundokuApp
+from tsundoku.config import (
+    ConfigCheckFailure,
+    GeneralConfig,
+    FeedsConfig,
+    TorrentConfig,
+    EncodeConfig,
+)
+
+
+async def test_async_retrieve_general_config(
+    app: MockTsundokuApp, caplog: LogCaptureFixture
+):
+    caplog.set_level(logging.ERROR, logger="tsundoku")
+
+    await GeneralConfig.retrieve(app)  # type: ignore
+
+
+async def test_async_retrieve_feeds_config(
+    app: MockTsundokuApp, caplog: LogCaptureFixture
+):
+    caplog.set_level(logging.ERROR, logger="tsundoku")
+
+    await FeedsConfig.retrieve(app)  # type: ignore
+
+
+async def test_async_retrieve_torrent_config(
+    app: MockTsundokuApp, caplog: LogCaptureFixture
+):
+    caplog.set_level(logging.ERROR, logger="tsundoku")
+
+    await TorrentConfig.retrieve(app)  # type: ignore
+
+
+async def test_async_retrieve_encode_config(
+    app: MockTsundokuApp, caplog: LogCaptureFixture
+):
+    caplog.set_level(logging.ERROR, logger="tsundoku")
+
+    await EncodeConfig.retrieve(app)  # type: ignore
+
+
+def test_sync_retrieve_general_config(app: MockTsundokuApp, caplog: LogCaptureFixture):
+    caplog.set_level(logging.ERROR, logger="tsundoku")
+
+    GeneralConfig.sync_retrieve(app)  # type: ignore
+
+
+def test_sync_retrieve_feeds_config(app: MockTsundokuApp, caplog: LogCaptureFixture):
+    caplog.set_level(logging.ERROR, logger="tsundoku")
+
+    FeedsConfig.sync_retrieve(app)  # type: ignore
+
+
+def test_sync_retrieve_torrent_config(app: MockTsundokuApp, caplog: LogCaptureFixture):
+    caplog.set_level(logging.ERROR, logger="tsundoku")
+
+    TorrentConfig.sync_retrieve(app)  # type: ignore
+
+
+def test_sync_retrieve_encode_config(app: MockTsundokuApp, caplog: LogCaptureFixture):
+    caplog.set_level(logging.ERROR, logger="tsundoku")
+
+    EncodeConfig.sync_retrieve(app)  # type: ignore
+
+
+async def test_general_config_invalid_port(
+    app: MockTsundokuApp, caplog: LogCaptureFixture
+):
+    caplog.set_level(logging.ERROR, logger="tsundoku")
+
+    config = await GeneralConfig.retrieve(app)  # type: ignore
+    config.port = 1023
+    with pytest.raises(ConfigCheckFailure):
+        await config.save()
+
+
+async def test_general_config_valid_port(
+    app: MockTsundokuApp, caplog: LogCaptureFixture
+):
+    caplog.set_level(logging.ERROR, logger="tsundoku")
+
+    config = await GeneralConfig.retrieve(app)  # type: ignore
+    config.port = 1024
+    await config.save()
